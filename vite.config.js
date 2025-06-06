ae65d15f5c82aee0c81462a2517a205d26e3e7d5
Mwvndva <111277645+Mwvndva@users.noreply.github.com>
2025-06-06 12:22:59 +0300

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   base: '/',
   server: {
     port: 8080,
     open: true,
-    // This handles the 404 redirects
-    historyApiFallback: true,
+  },
+  preview: {
+    port: 8080,
+    open: true,
   },
   build: {
     outDir: 'dist',
